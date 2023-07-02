6d99e206b81138b506871ed0ff6304c577da131a
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-07-02 21:14:35 +0300

    Вычисление центра масс

---
+++ Archimedes.Mechanics/MechanicsAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Archimedes.Mechanics
+{
+    public static class MechanicsAlgorithm
+    {
+        public static Vector2 CenterOfMass (double [] mass, Vector2 [] r)
+        {
+            double mx = 0.0, my = 0.0, m = 0.0;
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mx += mass [i] * r [i].X;
+                my += mass [i] * r [i].Y;
+                m  += mass [i];
+            }
+
+            return new Vector2 (mx / m, my / m);
+        }
+    }
+}
