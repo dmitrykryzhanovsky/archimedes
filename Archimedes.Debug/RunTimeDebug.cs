9385e2ce0d67bade79b445abe435aacf2d6ae5ec
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-07-03 22:27:16 +0300

    Так как я не могу запомнить, в каком порядке метод GetLength () выдаёт размеры массива, пришлось написать этот короткий тестик-напоминалку.

---
+++ Archimedes.Debug/RunTimeDebug.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Archimedes.Debug
+{
+    internal static class RunTimeDebug
+    {
+        /// <summary>
+        /// Так как я не могу запомнить, в каком порядке метод GetLength () выдаёт размеры массива, пришлось написать этот короткий 
+        /// тестик-напоминалку.
+        /// </summary>
+        internal static void MultidimensionalArray ()
+        {
+            int [,,,] array = new int [2, 3, 5, 7];
+
+            Console.WriteLine ("GetLength (0) = {0}", array.GetLength (0));
+            Console.WriteLine ("GetLength (1) = {0}", array.GetLength (1));
+            Console.WriteLine ("GetLength (2) = {0}", array.GetLength (2));
+            Console.WriteLine ("GetLength (3) = {0}", array.GetLength (3));
+            Console.WriteLine ("Length = {0}", array.Length);
+        }
+    }
+}
---
+++ Archimedes.Kernel/ArrayExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Archimedes
+{
+    public static class ArrayExtension
+    {
+        //public T [,] CopyTo<T> (this T [,] source, T [,] destination)
+        //{
+
+        //}
+    }
+}
