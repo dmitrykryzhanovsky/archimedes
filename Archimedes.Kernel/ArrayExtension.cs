a212f1f9d621be5137ae3a83f75440bd164b6955
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-07-03 22:40:39 +0300

    Добавил сигнатуры методов для ArrayExtension

--- Archimedes.Kernel/ArrayExtension.cs
+++ Archimedes.Kernel/ArrayExtension.cs
@@ -2,9 +2,134 @@
 {
     public static class ArrayExtension
     {
-        //public T [,] CopyTo<T> (this T [,] source, T [,] destination)
-        //{
+        public static T [,] CopyTo<T> (this T [] source, T [,] destination)
+        {
+            throw new NotImplementedException ();
+        }
 
-        //}
+        public static T [,] CopyTo<T> (this T [,] source, T [,] destination)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static bool Equals<T> (this T [] array1, T [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static bool Equals<T> (this T [,] array1, T [,] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [] Add (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] Add (this double [,] array1, double [,] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void AddTo (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void AddTo (this double [,] array1, double [,] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [] Subtract (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] Subtract (this double [,] array1, double [,] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void SubtractTo (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void SubtractTo (this double [,] array1, double [,] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [] Negate (this double [] array)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] Negate (this double [,] array)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void NegateTo (this double [] array)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void NegateTo (this double [,] array)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [] Multiply (this double [] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] Multiply (this double [,] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void MultiplyTo (this double [] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void MultiplyTo (this double [,] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [] Divide (this double [] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] Divide (this double [,] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void DivideTo (this double [] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static void DivideTo (this double [,] array, double coefficient)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double InnerProduct (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public static double [,] OuterProduct (this double [] array1, double [] array2)
+        {
+            throw new NotImplementedException ();
+        }
     }
 }
