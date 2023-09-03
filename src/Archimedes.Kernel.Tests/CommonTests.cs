5950719b0c3d37bed5e25316373c7b2325ea6090
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-09-03 11:19:57 +0300

    Тесты для разбиения единиц измерения на компоненты

--- src/Archimedes.Kernel.Tests/CommonTests.cs
+++ src/Archimedes.Kernel.Tests/CommonTests.cs
@@ -8,63 +8,108 @@ namespace Archimedes.Tests
     public class CommonTests
     {
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_2Components_Common ()
+        public void SplitNonNegativeIntoComponentsTest_2Components_Common ()
         {
             double x = 42.73;
             int unit0PerUnit1 = 60;
 
             (int unit0, double total1) expected = (42, 43.8);
 
-            (int unit0, double total1) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1);
+            (int unit0, double total1) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.total1, actual.total1, 1.0e-12);
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_2Components_DegZero ()
+        public void SplitNonNegativeIntoComponentsTest_2Components_DegZero ()
         {
             double x = 0.73;
             int unit0PerUnit1 = 60;
 
             (int unit0, double total1) expected = (0, 43.8);
 
-            (int unit0, double total1) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1);
+            (int unit0, double total1) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.total1, actual.total1);
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_2Components_DegZero_MinIntZero ()
+        public void SplitNonNegativeIntoComponentsTest_2Components_DegZero_MinIntZero ()
         {
             double x = 0.01;
             int unit0PerUnit1 = 60;
 
             (int unit0, double total1) expected = (0, 0.6);
 
-            (int unit0, double total1) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1);
+            (int unit0, double total1) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.total1, actual.total1);
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_2Components_Zero ()
+        public void SplitNonNegativeIntoComponentsTest_2Components_Zero ()
         {
             double x = 0.0;
             int unit0PerUnit1 = 60;
 
             (int unit0, double total1) expected = (0, 0.0);
 
-            (int unit0, double total1) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1);
+            (int unit0, double total1) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.total1, actual.total1);
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_3Components_Common ()
+        public void SplitIntoComponentsTest_2Components_Positive ()
+        {
+            double x = 73.426;
+            int unit0PerUnit1 = 60;
+
+            (ESign sign, int unit0, double total1) expected = (ESign.NonNegative, 73, 25.56);
+
+            (ESign sign, int unit0, double total1) actual = Common.SplitIntoComponents (x, unit0PerUnit1);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.total1, actual.total1, 1.0e-12);
+        }
+
+        [TestMethod ()]
+        public void SplitIntoComponentsTest_2Components_Zero ()
+        {
+            double x = 0.0;
+            int unit0PerUnit1 = 60;
+
+            (ESign sign, int unit0, double total1) expected = (ESign.NonNegative, 0, 0.0);
+
+            (ESign sign, int unit0, double total1) actual = Common.SplitIntoComponents (x, unit0PerUnit1);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.total1, actual.total1);
+        }
+
+        [TestMethod ()]
+        public void SplitIntoComponentsTest_2Components_Negative ()
+        {
+            double x = -0.426;
+            int unit0PerUnit1 = 60;
+
+            (ESign sign, int unit0, double total1) expected = (ESign.Negative, 0, 25.56);
+
+            (ESign sign, int unit0, double total1) actual = Common.SplitIntoComponents (x, unit0PerUnit1);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.total1, actual.total1);
+        }
+
+        [TestMethod ()]
+        public void SplitNonNegativeIntoComponentsTest_3Components_Common ()
         {
             double x = 42.35813;
             int unit0PerUnit1 = 60;
@@ -72,7 +117,7 @@ namespace Archimedes.Tests
 
             (int unit0, int unit1, double total2) expected = (42, 21, 29.268);
 
-            (int unit0, int unit1, double total2) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1,unit1PerUnit2);
+            (int unit0, int unit1, double total2) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.unit1, actual.unit1);
@@ -80,7 +125,7 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_3Components_DegZero ()
+        public void SplitNonNegativeIntoComponentsTest_3Components_DegZero ()
         {
             double x = 0.35813;
             int unit0PerUnit1 = 60;
@@ -88,7 +133,7 @@ namespace Archimedes.Tests
 
             (int unit0, int unit1, double total2) expected = (0, 21, 29.268);
 
-            (int unit0, int unit1, double total2) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+            (int unit0, int unit1, double total2) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.unit1, actual.unit1);
@@ -96,7 +141,7 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_3Components_DegZeroMinZero ()
+        public void SplitNonNegativeIntoComponentsTest_3Components_DegZeroMinZero ()
         {
             double x = 0.00813;
             int unit0PerUnit1 = 60;
@@ -104,7 +149,7 @@ namespace Archimedes.Tests
 
             (int unit0, int unit1, double total2) expected = (0, 0, 29.268);
 
-            (int unit0, int unit1, double total2) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+            (int unit0, int unit1, double total2) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.unit1, actual.unit1);
@@ -112,7 +157,7 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_3Components_DegZeroMinZeroSecIntZero ()
+        public void SplitNonNegativeIntoComponentsTest_3Components_DegZeroMinZeroSecIntZero ()
         {
             double x = 0.000075;
             int unit0PerUnit1 = 60;
@@ -120,7 +165,7 @@ namespace Archimedes.Tests
 
             (int unit0, int unit1, double total2) expected = (0, 0, 0.27);
 
-            (int unit0, int unit1, double total2) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+            (int unit0, int unit1, double total2) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.unit1, actual.unit1);
@@ -128,7 +173,7 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void SplitPositiveIntoComponentsTest_3Components_Zero ()
+        public void SplitNonNegativeIntoComponentsTest_3Components_Zero ()
         {
             double x = 0.0;
             int unit0PerUnit1 = 60;
@@ -136,11 +181,62 @@ namespace Archimedes.Tests
 
             (int unit0, int unit1, double total2) expected = (0, 0, 0.0);
 
-            (int unit0, int unit1, double total2) actual = Common.SplitPositiveIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+            (int unit0, int unit1, double total2) actual = Common.SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
 
             Assert.AreEqual (expected.unit0, actual.unit0);
             Assert.AreEqual (expected.unit1, actual.unit1);
             Assert.AreEqual (expected.total2, actual.total2, 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void SplitIntoComponentsTest_3Components_Common ()
+        {
+            double x = 0.426;
+            int unit0PerUnit1 = 60;
+            int unit1PerUnit2 = 60;
+
+            (ESign sign, int unit0, int unit1, double total2) expected = (ESign.NonNegative, 0, 25, 33.6);
+
+            (ESign sign, int unit0, int unit1, double total2) actual = Common.SplitIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.unit1, actual.unit1);
+            Assert.AreEqual (expected.total2, actual.total2, 1.0e-11);
+        }
+
+        [TestMethod ()]
+        public void SplitIntoComponentsTest_3Components_Zero ()
+        {
+            double x = 0.0;
+            int unit0PerUnit1 = 60;
+            int unit1PerUnit2 = 60;
+
+            (ESign sign, int unit0, int unit1, double total2) expected = (ESign.NonNegative, 0, 0, 0.0);
+
+            (ESign sign, int unit0, int unit1, double total2) actual = Common.SplitIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.unit1, actual.unit1);
+            Assert.AreEqual (expected.total2, actual.total2, 1.0e-11);
+        }
+
+        [TestMethod ()]
+        public void SplitIntoComponentsTest_3Components_Negative ()
+        {
+            double x = -0.009;
+            int unit0PerUnit1 = 60;
+            int unit1PerUnit2 = 60;
+
+            (ESign sign, int unit0, int unit1, double total2) expected = (ESign.Negative, 0, 0, 32.4);
+
+            (ESign sign, int unit0, int unit1, double total2) actual = Common.SplitIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+
+            Assert.AreEqual (expected.sign, actual.sign);
+            Assert.AreEqual (expected.unit0, actual.unit0);
+            Assert.AreEqual (expected.unit1, actual.unit1);
+            Assert.AreEqual (expected.total2, actual.total2, 1.0e-11);
         }
     }
 }
--- src/Archimedes.Kernel/Common.cs
+++ src/Archimedes.Kernel/Common.cs
@@ -2,7 +2,32 @@
 {
     public static class Common
     {
-        public static (int unit0, double total1) SplitPositiveIntoComponents (double x, int unit0PerUnit1)
+        public static (ESign sign, int unit0, double total1) SplitIntoComponents (double x, int unit0PerUnit1)
+        {
+            ESign  sign;
+            int    unit0;
+            double total1;
+
+            if (x >= 0.0) sign = ESign.NonNegative;
+
+            else
+            {
+                sign = ESign.Negative;
+                x    = -x;
+            }
+
+            (unit0, total1) = SplitNonNegativeIntoComponents (x, unit0PerUnit1);
+
+            return (sign, unit0, total1);
+        }
+
+        /// <summary>
+        /// Разделят неотрицательную величину x на два компонента (например, градусы на целое число градусов и минуты).
+        /// </summary>
+        /// <param name="unit0PerUnit1">Сколько единиц измерения младшего порядка содержится в одной единице измерения старшого порядка 
+        /// (например, сколько минут в одном градусе).</param>
+        /// <returns>На примере градусов: для x = 73.426°, unit0 = 73°, total1 = 25.56ʹ (что соответствует 0.426°).</returns>
+        public static (int unit0, double total1) SplitNonNegativeIntoComponents (double x, int unit0PerUnit1)
         {
             int    unit0  = (int)x;
             double total1 = (x - unit0) * unit0PerUnit1;
@@ -10,7 +35,35 @@
             return (unit0, total1);
         }
 
-        public static (int unit0, int unit1, double total2) SplitPositiveIntoComponents (double x, int unit0PerUnit1, int unit1PerUnit2)
+        public static (ESign sign, int unit0, int unit1, double total2) SplitIntoComponents (double x, int unit0PerUnit1, int unit1PerUnit2)
+        {
+            ESign  sign;
+            int    unit0;
+            int    unit1;
+            double total2;
+
+            if (x >= 0.0) sign = ESign.NonNegative;
+
+            else
+            {
+                sign = ESign.Negative;
+                x    = -x;
+            }
+
+            (unit0, unit1, total2) = SplitNonNegativeIntoComponents (x, unit0PerUnit1, unit1PerUnit2);
+
+            return (sign, unit0, unit1, total2);
+        }
+
+        /// <summary>
+        /// Разделят неотрицательную величину x на три компонента (например, градусы на целое число градусов, целое число минут и секунды).
+        /// </summary>
+        /// <param name="unit0PerUnit1">Сколько единиц измерения первого порядка содержится в одной единице измерения нулевого порядка 
+        /// (например, сколько минут в одном градусе).</param>
+        /// <param name="unit1PerUnit2">Сколько единиц измерения второго порядка содержится в одной единице измерения первого порядка 
+        /// (например, сколько секунд в одной минуте).</param>
+        /// <returns>На примере градусов: для x = 73.426°, unit0 = 73°, unit1 = 25ʹ, total2 = 33.6ʺ.</returns>
+        public static (int unit0, int unit1, double total2) SplitNonNegativeIntoComponents (double x, int unit0PerUnit1, int unit1PerUnit2)
         {
             int    unit0  = (int)x;
             double total1 = (x - unit0) * unit0PerUnit1;
