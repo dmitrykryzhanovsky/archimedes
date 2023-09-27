59f96166a93b0a9d923667d1bed5245e41b9aa06
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-09-27 22:46:58 +0300

    Тесты и code review для матричных и векторных алгоритмов

--- src/Archimedes.Kernel/VectorAlgorithm.cs
+++ src/Archimedes.Kernel/VectorAlgorithm.cs
@@ -2,6 +2,11 @@
 {
     public static class VectorAlgorithm
     {
+        /// <summary>
+        /// Возвращает true, если вектора по своим размерам подходят для алгебраических операций (сложения, вычитания, скалярного 
+        /// произведения). В противном случае false.
+        /// </summary>
+        /// <remarks>Метод проверяет, равна ли размерность вектора v1 размерности вектора v2.</remarks>
         public static bool AreSuitableForAlgebra (Vector v1, Vector v2)
         {
             return (v1.Dimension == v2.Dimension);
