a09b12f967f9fb4e6624b494bfa52052088e270e
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-09-30 17:02:22 +0300

    Написал классы Polar2 и Polar3

---
+++ src/Archimedes.Kernel/Polar2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Archimedes
+{
+    public class Polar2 : ICloneable
+    {
+        private double _r;
+        private double _heading;
+
+        public double R
+        {
+            get => _r;
+        }
+
+        public double Heading
+        {
+            get => _heading;
+        }
+
+        private Polar2 ()
+        {
+        }
+
+        public Polar2 (Polar2 other)
+        {
+            _r       = other._r;
+            _heading = other._heading;
+        }
+
+        public object Clone ()
+        {
+            return new Polar2 (this);
+        }
+
+        public static Polar2 CartesianToPolar (Vector2 v)
+        {
+            Polar2 p = new Polar2 ();
+
+            p._r       = v.GetLength ();
+            p._heading = v.GetHeading ();
+
+            return p;
+        }
+
+        public static Vector2 PolarToCartesian (Polar2 p)
+        {
+            (double sin, double cos) = Math.SinCos (p._heading);
+
+            return new Vector2 (p._r * cos, p._r * sin);
+        }
+    }
+}
---
+++ src/Archimedes.Kernel/Polar3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Archimedes
+{
+    public class Polar3 : ICloneable
+    {
+        private double _r;
+        private double _longitude;
+        private double _latitude;
+
+        public double R
+        {
+            get => _r;
+        }
+
+        public double Longitude
+        {
+            get => _longitude;
+        }
+
+        public double Latitude
+        {
+            get => _latitude;
+        }
+
+        private Polar3 ()
+        {
+        }
+
+        public Polar3 (Polar3 other)
+        {
+            _r         = other._r;
+            _longitude = other._longitude;
+            _latitude  = other._latitude;
+        }
+
+        public object Clone ()
+        {
+            return new Polar3 (this);
+        }
+
+        public static Polar3 CartesianToPolar (Vector3 v)
+        {
+            Polar3 p = new Polar3 ();
+
+            p._r         = v.GetLength ();
+            p._longitude = Math.Atan2 (v.Y, v.X);
+            p._latitude  = Math.Asin (v.Z / p._r);
+
+            return p;
+        }
+
+        public static Vector3 PolarToCartesian (Polar3 p)
+        {
+            (double sinB, double cosB) = Math.SinCos (p._latitude);
+            (double sinL, double cosL) = Math.SinCos (p._longitude);
+
+            return new Vector3 (p._r * cosB * cosL, 
+                                p._r * cosB * sinL, 
+                                p._r * sinB);
+        }
+    }
+}
