f0ef3a80e34145895a27494604e4255e72e214e0
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-10-07 21:38:25 +0300

    Делал матрицы поворота

---
+++ src/Archimedes.Kernel.Tests/SpaceTests.cs
@@ -0,0 +1,173 @@
+﻿using Archimedes;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class SpaceTests
+    {
+        [TestMethod ()]
+        public void RotationAroundOXTest_Angle ()
+        {
+            double phi = Math.PI / 3.0;
+
+            Matrix3 expected = new Matrix3 (1.0, 0.0, 0.0,
+                                            0.0, 0.5, Math.Sqrt (3.0) / 2.0,
+                                            0.0, -Math.Sqrt (3.0) / 2.0, 0.5);
+
+            Matrix3 actual = Space.RotationAroundOX (phi);
+
+            Assert.AreEqual (expected [0, 0], actual [0, 0]);
+            Assert.AreEqual (expected [0, 1], actual [0, 1]);
+            Assert.AreEqual (expected [0, 2], actual [0, 2]);
+
+            Assert.AreEqual (expected [1, 0], actual [1, 0]);
+            Assert.AreEqual (expected [1, 1], actual [1, 1], 1.0e-15);
+            Assert.AreEqual (expected [1, 2], actual [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expected [2, 0], actual [2, 0]);
+            Assert.AreEqual (expected [2, 1], actual [2, 1], 1.0e-15);
+            Assert.AreEqual (expected [2, 2], actual [2, 2], 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void RotationAroundOXTest_SineCosine ()
+        {
+            double sin = 0.86602540378443865;
+            double cos = 0.5;
+
+            Matrix3 expected = new Matrix3 (1.0, 0.0, 0.0,
+                                            0.0, 0.5, Math.Sqrt (3.0) / 2.0,
+                                            0.0, -Math.Sqrt (3.0) / 2.0, 0.5);
+
+            Matrix3 actual = Space.RotationAroundOX (sin, cos);
+
+            Assert.AreEqual (expected [0, 0], actual [0, 0]);
+            Assert.AreEqual (expected [0, 1], actual [0, 1]);
+            Assert.AreEqual (expected [0, 2], actual [0, 2]);
+
+            Assert.AreEqual (expected [1, 0], actual [1, 0]);
+            Assert.AreEqual (expected [1, 1], actual [1, 1], 1.0e-15);
+            Assert.AreEqual (expected [1, 2], actual [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expected [2, 0], actual [2, 0]);
+            Assert.AreEqual (expected [2, 1], actual [2, 1], 1.0e-15);
+            Assert.AreEqual (expected [2, 2], actual [2, 2], 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void RotationAroundOXBothTest_Angle ()
+        {
+            double phi = Math.PI / 3.0;
+
+            Matrix3 expectedDirect = new Matrix3 (1.0, 0.0, 0.0,
+                                                  0.0, 0.5, Math.Sqrt (3.0) / 2.0,
+                                                  0.0, -Math.Sqrt (3.0) / 2.0, 0.5);
+
+            Matrix3 expectedBackward = new Matrix3 (1.0, 0.0, 0.0,
+                                                    0.0, 0.5, -Math.Sqrt (3.0) / 2.0,
+                                                    0.0, Math.Sqrt (3.0) / 2.0, 0.5);
+
+            (Matrix3 actualDirect, Matrix3 actualBackward) = Space.RotationAroundOXBoth (phi);
+
+            Assert.AreEqual (expectedDirect [0, 0], actualDirect [0, 0]);
+            Assert.AreEqual (expectedDirect [0, 1], actualDirect [0, 1]);
+            Assert.AreEqual (expectedDirect [0, 2], actualDirect [0, 2]);
+
+            Assert.AreEqual (expectedDirect [1, 0], actualDirect [1, 0]);
+            Assert.AreEqual (expectedDirect [1, 1], actualDirect [1, 1], 1.0e-15);
+            Assert.AreEqual (expectedDirect [1, 2], actualDirect [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedDirect [2, 0], actualDirect [2, 0]);
+            Assert.AreEqual (expectedDirect [2, 1], actualDirect [2, 1], 1.0e-15);
+            Assert.AreEqual (expectedDirect [2, 2], actualDirect [2, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedBackward [0, 0], actualBackward [0, 0]);
+            Assert.AreEqual (expectedBackward [0, 1], actualBackward [0, 1]);
+            Assert.AreEqual (expectedBackward [0, 2], actualBackward [0, 2]);
+
+            Assert.AreEqual (expectedBackward [1, 0], actualBackward [1, 0]);
+            Assert.AreEqual (expectedBackward [1, 1], actualBackward [1, 1], 1.0e-15);
+            Assert.AreEqual (expectedBackward [1, 2], actualBackward [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedBackward [2, 0], actualBackward [2, 0]);
+            Assert.AreEqual (expectedBackward [2, 1], actualBackward [2, 1], 1.0e-15);
+            Assert.AreEqual (expectedBackward [2, 2], actualBackward [2, 2], 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void RotationAroundOXBothTest_SineCosine ()
+        {
+            double sin = 0.86602540378443865;
+            double cos = 0.5;
+
+            Matrix3 expectedDirect = new Matrix3 (1.0, 0.0, 0.0,
+                                                  0.0, 0.5, Math.Sqrt (3.0) / 2.0,
+                                                  0.0, -Math.Sqrt (3.0) / 2.0, 0.5);
+
+            Matrix3 expectedBackward = new Matrix3 (1.0, 0.0, 0.0,
+                                                    0.0, 0.5, -Math.Sqrt (3.0) / 2.0,
+                                                    0.0, Math.Sqrt (3.0) / 2.0, 0.5);
+
+            (Matrix3 actualDirect, Matrix3 actualBackward) = Space.RotationAroundOXBoth (sin, cos);
+
+            Assert.AreEqual (expectedDirect [0, 0], actualDirect [0, 0]);
+            Assert.AreEqual (expectedDirect [0, 1], actualDirect [0, 1]);
+            Assert.AreEqual (expectedDirect [0, 2], actualDirect [0, 2]);
+
+            Assert.AreEqual (expectedDirect [1, 0], actualDirect [1, 0]);
+            Assert.AreEqual (expectedDirect [1, 1], actualDirect [1, 1], 1.0e-15);
+            Assert.AreEqual (expectedDirect [1, 2], actualDirect [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedDirect [2, 0], actualDirect [2, 0]);
+            Assert.AreEqual (expectedDirect [2, 1], actualDirect [2, 1], 1.0e-15);
+            Assert.AreEqual (expectedDirect [2, 2], actualDirect [2, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedBackward [0, 0], actualBackward [0, 0]);
+            Assert.AreEqual (expectedBackward [0, 1], actualBackward [0, 1]);
+            Assert.AreEqual (expectedBackward [0, 2], actualBackward [0, 2]);
+
+            Assert.AreEqual (expectedBackward [1, 0], actualBackward [1, 0]);
+            Assert.AreEqual (expectedBackward [1, 1], actualBackward [1, 1], 1.0e-15);
+            Assert.AreEqual (expectedBackward [1, 2], actualBackward [1, 2], 1.0e-15);
+
+            Assert.AreEqual (expectedBackward [2, 0], actualBackward [2, 0]);
+            Assert.AreEqual (expectedBackward [2, 1], actualBackward [2, 1], 1.0e-15);
+            Assert.AreEqual (expectedBackward [2, 2], actualBackward [2, 2], 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void RotateVectorAroundOXTest_Angle ()
+        {
+            Vector3 v = new Vector3 (0, 1, 1.7320508075688773);
+            double phi = -0.52359877559829887;
+
+            Vector3 expected = new Vector3 (0, 1.7320508075688773, 1);
+
+            Vector3 actual = Space.RotateVectorAroundOX (v, phi);
+
+            Assert.AreEqual (expected.X, actual.X);
+            Assert.AreEqual (expected.Y, actual.Y);
+            Assert.AreEqual (expected.Z, actual.Z, 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void RotateVectorAroundOXTest_SineCosine ()
+        {
+            Vector3 v = new Vector3 (0, 1, 1.7320508075688773);
+            double sin = 0.5;
+            double cos = 0.86602540378443865;
+
+            Vector3 expected = new Vector3 (0, 0, 2);
+
+            Vector3 actual = Space.RotateVectorAroundOX (v, sin, cos);
+
+            Assert.AreEqual (expected.X, actual.X);
+            Assert.AreEqual (expected.Y, actual.Y);
+            Assert.AreEqual (expected.Z, actual.Z, 1.0e-15);
+        }
+    }
+}
---
+++ src/Archimedes.Kernel/Space.cs
@@ -0,0 +1,225 @@
+﻿using System;
+
+namespace Archimedes
+{
+    public static class Space
+    {
+        /// <summary>
+        /// Вычисляет матрицу поворота вокруг оси OX на угол phi.
+        /// </summary>
+        public static Matrix3 RotationAroundOX (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOX (sin, cos);
+        }
+
+        /// <summary>
+        /// Вычисляет матрицу поворота вокруг оси OX на угол, заданный синусом sin и косинусом cos.
+        /// </summary>
+        /// <remarks>Проверка значений sin и cos на соответствие основному тригонометрическому тождеству в методе не производится.</remarks>
+        public static Matrix3 RotationAroundOX (double sin, double cos)
+        {
+            return new Matrix3 (1.0,  0.0, 0.0,
+                                0.0,  cos, sin,
+                                0.0, -sin, cos);
+        }
+
+        /// <summary>
+        /// Вычисляет матрицы поворота вокруг оси OX на угол phi и угол -phi (т.е. в противоположном направлении).
+        /// </summary>
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOXBoth (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOXBoth (sin, cos);
+        }
+
+        /// <summary>
+        /// Вычисляет матрицы поворота вокруг оси OX на угол, заданный синусом sin и косинусом cos, и равный ему по модулю угол, но в 
+        /// противоположном направлении.
+        /// </summary>
+        /// <remarks>Проверка значений sin и cos на соответствие основному тригонометрическому тождеству в методе не производится.</remarks>
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOXBoth (double sin, double cos)
+        {
+            Matrix3 direct = new Matrix3 (1.0,  0.0, 0.0,
+                                          0.0,  cos, sin,
+                                          0.0, -sin, cos);
+
+            Matrix3 backward = new Matrix3 (1.0, 0.0,  0.0,
+                                            0.0, cos, -sin,
+                                            0.0, sin,  cos);
+
+            return (direct, backward);
+        }
+
+        /// <summary>
+        /// Поворот вектора v вокруг оси OX на угол phi.
+        /// </summary>
+        public static Vector3 RotateVectorAroundOX (Vector3 v, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateVectorAroundOX (v, sin, cos);
+        }
+
+        /// <summary>
+        /// Поворот вектора v вокруг оси OX на угол phi, заданный синусом sin и косинусом cos.
+        /// </summary>
+        /// <remarks>Проверка значений sin и cos на соответствие основному тригонометрическому тождеству в методе не производится.</remarks>
+        public static Vector3 RotateVectorAroundOX (Vector3 v, double sin, double cos)
+        {
+            return new Vector3 (v.X,
+                                v.Y * cos - v.Z * sin,
+                                v.Y * sin + v.Z * cos);
+        }
+
+        public static Vector3 RotateReferenceAroundOX (Vector3 v, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateReferenceAroundOX (v, sin, cos);
+        }
+
+        public static Vector3 RotateReferenceAroundOX (Vector3 v, double sin, double cos)
+        {
+            return new Vector3 ( v.X,
+                                 v.Y * cos + v.Z * sin,
+                                -v.Y * sin + v.Z * cos);
+        }
+
+        public static Polar3 RotateReferenceAroundOX (Polar3 p, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateReferenceAroundOX (p, sin, cos);
+        }
+
+        public static Polar3 RotateReferenceAroundOX (Polar3 p, double sin, double cos)
+        {
+            (double sinb, double cosb) = Math.SinCos (p.Latitude);
+            (double sinl, double cosl) = Math.SinCos (p.Longitude);
+
+            return new Polar3 (p.R,
+                               Math.Atan2 (cosb * sinl * cos + sinb * sin, cosb * cosl),
+                               Math.Asin (-cosb * sinl * sin + sinb * cos));
+        }
+
+        public static (double phi2, double l2) RotateReferenceAroundOX (double phi1, double l1, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateReferenceAroundOX (phi1, l1, sin, cos);
+        }
+
+        public static (double phi2, double l2) RotateReferenceAroundOX (double phi1, double l1, double sin, double cos)
+        {
+            (double sinb, double cosb) = Math.SinCos (phi1);
+            (double sinl, double cosl) = Math.SinCos (l1);
+
+            return (Math.Asin (-cosb * sinl * sin + sinb * cos),
+                    Math.Atan2 (cosb * sinl * cos + sinb * sin, cosb * cosl));
+        }
+
+        public static Matrix3 RotationAroundOY (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOY (sin, cos);
+        }
+
+        public static Matrix3 RotationAroundOY (double sin, double cos)
+        {
+            return new Matrix3 (cos, 0.0, -sin,
+                                0.0, 1.0,  0.0,
+                                sin, 0.0,  cos);
+        }
+
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOYBoth (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOYBoth (sin, cos);
+        }
+
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOYBoth (double sin, double cos)
+        {
+            Matrix3 direct = new Matrix3 (cos, 0.0, -sin,
+                                          0.0, 1.0,  0.0,
+                                          sin, 0.0,  cos);
+
+            Matrix3 backward = new Matrix3 ( cos, 0.0, sin,
+                                             0.0, 1.0, 0.0,
+                                            -sin, 0.0, cos);
+
+            return (direct, backward);
+        }
+
+        public static Vector3 RotateVectorAroundOY (Vector3 v, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateVectorAroundOY (v, sin, cos);
+        }
+
+        public static Vector3 RotateVectorAroundOY (Vector3 v, double sin, double cos)
+        {
+            return new Vector3 ( v.X * cos + v.Z * sin, 
+                                 v.Y, 
+                                -v.X * sin + v.Z * cos);
+        }
+
+        public static Matrix3 RotationAroundOZ (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOZ (sin, cos);
+        }
+
+        public static Matrix3 RotationAroundOZ (double sin, double cos)
+        {
+            return new Matrix3 ( cos, sin, 0.0,
+                                -sin, cos, 0.0,
+                                 0.0, 0.0, 1.0);
+        }
+
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOZBoth (double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotationAroundOZBoth (sin, cos);
+        }
+
+        public static (Matrix3 direct, Matrix3 backward) RotationAroundOZBoth (double sin, double cos)
+        {
+            Matrix3 direct = new Matrix3 ( cos, sin, 0.0,
+                                          -sin, cos, 0.0,
+                                           0.0, 0.0, 1.0);
+
+            Matrix3 backward = new Matrix3 (cos, -sin, 0.0,
+                                            sin,  cos, 0.0,
+                                            0.0,  0.0, 1.0);
+
+            return (direct, backward);
+        }
+
+        public static Vector3 RotateVectorAroundOZ (Vector3 v, double phi)
+        {
+            (double sin, double cos) = Math.SinCos (phi);
+
+            return RotateVectorAroundOZ (v, sin, cos);
+        }
+
+        public static Vector3 RotateVectorAroundOZ (Vector3 v, double sin, double cos)
+        {
+            return new Vector3 (v.X * cos - v.Y * sin,
+                                v.X * sin + v.Y * cos,
+                                v.Z);
+        }
+
+        public static Vector3 RotateVector (Vector3 v, Matrix3 rotationMatrix)
+        {
+            return rotationMatrix * v;
+        }
+    }
+}
