3111cd214636ae513ed328d32d0966b848ec8288
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-10-27 07:11:00 +0300

    Начал код InsertionSort

---
+++ src/Archimedes.Kernel/Sort.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Archimedes
+{
+    public static class Sort
+    {
+        public static void InsertionSort<T> (this T [] array) where T : INumber<T>
+        {
+
+        }
+    }
+}
