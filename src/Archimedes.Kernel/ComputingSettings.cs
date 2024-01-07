8a1f9ead4a1812ca09b7b87b9569efca2aaae3fe
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-01-07 13:18:36 +0300

    Рефакторинг

--- src/Archimedes.Kernel/ComputingSettings.cs
+++ src/Archimedes.Kernel/ComputingSettings.cs
@@ -3,8 +3,13 @@
     /// <summary>
     /// Настройки вычислений.
     /// </summary>
-    internal static class ComputingSettings
+    public static class ComputingSettings
     {
+        /// <summary>
+        /// Точность вычислений с типом double по-умолчанию.
+        /// </summary>
+        public const double DoublePrecision = 1.0e-16;
+
         /// <summary>
         /// Если используется этот параметр, то числа, меньшие чем ZeroAccuracy, будут считаться равными 0.
         /// </summary>
