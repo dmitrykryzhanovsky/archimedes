ce96b9b013cdc0956a687aae5850569c517681a1
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-02-04 08:48:10 +0300

    Написал по-быстрому бенчмарк для Selection, Insertion и Merge sort, чтобы найти оптимальный чанк ля сортировки слиянием и посмотреть, как ведёт себя память
    По результатам данного бенчмарка оптимальный чанк = 32 или 64

---
+++ src/Archimedes.Kernel.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Archimedes.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main (string [] args)
+        {
+            BenchmarkRunner.Run<SortBenchmark> ();
+        }
+    }
+}
