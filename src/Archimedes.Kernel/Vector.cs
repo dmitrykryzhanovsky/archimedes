4173924ec184ae389e99191364ac3bd01e76c5ec
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-02-11 08:03:54 +0300

    Рефакторинг

---
+++ src/Archimedes.Kernel/Vector.cs
@@ -0,0 +1,28 @@
+﻿namespace Archimedes
+{
+    public class Vector
+    {
+        protected readonly double [] _x;
+
+        public double this [int index]
+        {
+            get => _x [index];
+            set => _x [index] = value;
+        }
+
+        public double [] Components
+        {
+            get => _x;
+        }
+
+        public virtual int Dimension
+        {
+            get => _x.Length;
+        }
+
+        protected Vector (int dimension)
+        {
+            _x = new double [dimension];
+        }
+    }
+}
---
+++ src/Archimedes.Kernel/Vector3.cs
@@ -0,0 +1,70 @@
+﻿namespace Archimedes
+{
+    public class Vector3 : Vector, ICloneable, IEquatable<Vector3>
+    {
+        private const int Size = 3;
+
+        public override int Dimension
+        {
+            get => Size;
+        }
+
+        public double X
+        {
+            get => _x [0];
+            set => _x [0] = value;
+        }
+
+        public double Y
+        {
+            get => _x [1];
+            set => _x [1] = value;
+        }
+
+        public double Z
+        {
+            get => _x [2];
+            set => _x [2] = value;
+        }
+
+        public Vector3 (double x, double y, double z) : base (Size)
+        {
+            _x [0] = x;
+            _x [1] = y;
+            _x [2] = z;
+        }
+
+        public Vector3 (Vector3 other) : this (other.X, other.Y, other.Z)
+        {
+        }
+
+        public object Clone ()
+        {
+            return new Vector3 (this);
+        }
+
+        public override bool Equals (object? obj)
+        {
+            if (obj is Vector3) return Equals (obj as Vector3);
+
+            else return base.Equals (obj);
+        }
+
+        public bool Equals (Vector3? other)
+        {
+            return ((X == other.X) && 
+                    (Y == other.Y) && 
+                    (Z == other.Z));
+        }
+
+        public static bool operator == (Vector3 v1, Vector3 v2)
+        {
+            return v1.Equals (v2);
+        }
+
+        public static bool operator != (Vector3 v1, Vector3 v2)
+        {
+            return v1.Equals (v2);
+        }
+    }
+}
