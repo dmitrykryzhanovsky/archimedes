16eb214d9f82c1aa6d1dc8e6d3ad9a79533bb5fb
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-02-14 21:27:29 +0300

    Сделал схему Горнера

---
+++ src/Archimedes.Kernel.Tests/PolynomialTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class PolynomialTests
+    {
+        [TestMethod ()]
+        public void ComputeTest ()
+        {
+            double [] a = new double [] { 2, -3, 5, 8, -13 };
+            double    x = 2;
+
+            double expected = -128.0;
+
+            double actual = Polynomial.Compute (a, x);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
