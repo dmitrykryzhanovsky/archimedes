8ae835b232f71ebf7cf66dc544d35f0d903a6557
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-04-14 18:36:11 +0300

    Начал переносить пирамидальную сортировку

--- src/Archimedes.Core/ArrayExtension.cs
+++ src/Archimedes.Core/ArrayExtension.cs
@@ -21,5 +21,59 @@ namespace Archimedes
 
             else return false;
         }
+
+        /// <summary>
+        /// Возвращает индекс, который соответствует наибольшему из элементов массива <paramref name="array"/> [<paramref name="index1"/>] 
+        /// и <paramref name="array"/> [<paramref name="index2"/>].
+        /// </summary>
+        /// <remarks>Если array [index1] = array [index2], то возвращается <paramref name="index1"/>.</remarks>
+        public static int MaxIndex<T> (this T [] array, int index1, int index2) where T : INumber<T>
+        {
+            return (array [index1] >= array [index2]) ? index1 : index2;
+        }
+
+        /// <summary>
+        /// Возвращает индекс, который соответствует наибольшему из элементов массива <paramref name="array"/> [<paramref name="index1"/>], 
+        /// <paramref name="array"/> [<paramref name="index2"/>] и <paramref name="array"/> [<paramref name="index3"/>].
+        /// </summary>
+        /// <remarks><list type="bullet">
+        /// <item>Если array [index1] = array [index2] = array [index3], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index1] = array [index2] > array [index3], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index1] = array [index3] > array [index2], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index2] = array [index3] > array [index1], возвращается <paramref name="index2"/>.</item>
+        /// </list></remarks>
+        public static int MaxIndex<T> (this T [] array, int index1, int index2, int index3) where T : INumber<T>
+        {
+            int maxIndex12 = array.MaxIndex (index1, index2);
+
+            return array.MaxIndex (maxIndex12, index3);
+        }
+
+        /// <summary>
+        /// Возвращает индекс, который соответствует наименьшему из элементов массива <paramref name="array"/> [<paramref name="index1"/>] 
+        /// и <paramref name="array"/> [<paramref name="index2"/>].
+        /// </summary>
+        /// <remarks>Если array [index1] = array [index2], то возвращается <paramref name="index2"/>.</remarks>
+        public static int MinIndex<T> (this T [] array, int index1, int index2) where T : INumber<T>
+        {
+            return (array [index1] <= array [index2]) ? index1 : index2;
+        }
+
+        /// <summary>
+        /// Возвращает индекс, который соответствует наименьшему из элементов массива <paramref name="array"/> [<paramref name="index1"/>], 
+        /// <paramref name="array"/> [<paramref name="index2"/>] и <paramref name="array"/> [<paramref name="index3"/>].
+        /// </summary>
+        /// <remarks><list type="bullet">
+        /// <item>Если array [index1] = array [index2] = array [index3], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index1] = array [index2] > array [index3], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index1] = array [index3] > array [index2], возвращается <paramref name="index1"/>.</item>
+        /// <item>Если array [index2] = array [index3] > array [index1], возвращается <paramref name="index2"/>.</item>
+        /// </list></remarks>
+        public static int MinIndex<T> (this T [] array, int index1, int index2, int index3) where T : INumber<T>
+        {
+            int minIndex12 = array.MinIndex (index1, index2);
+
+            return array.MinIndex (minIndex12, index3);
+        }
     }
 }
---
+++ src/Archimedes.Core/HeapAlgorithm.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace Archimedes
+{
+    public static class HeapAlgorithm
+    {
+        /// <summary>
+        /// Поддержание свойства невозрастающей пирамиды для поддерева с корнем в индексе <paramref name="subtreeRootIndex"/>.
+        /// </summary>
+        /// <param name="firstLeafIndex">Индекс первого листа пирамиды.</param>
+        /// <param name="lastLeafIndex">Индекс последнего элемента пирамиды.</param>
+        /// <remarks>Этот метод предназначен не для построения невозрастающей пирамиды с корнем в индексе <paramref name="subtreeRootIndex"/>, 
+        /// а именно для поддержания свойства невозрастания для поддерева с корнем в индексе <paramref name="subtreeRootIndex"/>. Он будет 
+        /// корректно работать только в том случае, если левое и правое дочерние поддеревья для элемента с индексом 
+        /// <paramref name="subtreeRootIndex"/> уже поддерживают свойство невозрастания.</remarks>
+        public static void MaxHeapify<T> (this T [] array, int subtreeRootIndex, int firstLeafIndex, int lastLeafIndex) where T : INumber<T>
+        {
+            int i = subtreeRootIndex;
+
+            // Спускаемся по пирамиде, пока не будут достигнуты листья (элементы, не имеющие дочерних) или пока в поддереве с корнем в
+            // индексе subtreeRootIndex не будет поддерживаться свойство невозрастания.
+            while (i < firstLeafIndex)
+            {
+                int leftChildIndex = GetLeftChildIndex (i);
+                int largestIndex   = (leftChildIndex < lastLeafIndex) ? array.MaxIndex (i, leftChildIndex, leftChildIndex + 1) :
+                                                                        array.MaxIndex (i, leftChildIndex);
+
+                // Если элемент с индексом i нарушил порядок невозрастания, проталкиваем его вниз.
+                if (array [largestIndex] > array [i])
+                {
+                    array.Swap (i, largestIndex);
+
+                    i = largestIndex;
+                }
+
+                // Если элемент с индексом i не нарушил порядок невозрастания, свойство невозрастания поддерживается, выходим из цикла.
+                else break;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает индекс левого потомка для элемента с индексом <paramref name="index"/>.
+        /// </summary>
+        public static int GetLeftChildIndex (int index)
+        {
+            return (index << 1) + 1;
+        }
+    }
+}
