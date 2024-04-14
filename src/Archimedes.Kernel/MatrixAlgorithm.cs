f1dbfb53fa333d363d474235a2b986708231d13a
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-04-14 09:15:40 +0300

    Неоптимально написан код

--- src/Archimedes.Kernel/MatrixAlgorithm.cs
+++ src/Archimedes.Kernel/MatrixAlgorithm.cs
@@ -4,7 +4,25 @@
     {
         public static Matrix ConventionalMultiplication (Matrix m1, Matrix m2)
         {
-            throw new NotImplementedException ();
+            Matrix result     = new Matrix (m1.Height, m2.Width);
+            int    commonSize = m1.Width;
+
+            for (int i = 0; i < m1.Height; i++)
+            {
+                for (int j = 0; j < m2.Width; j++)
+                {
+                    double c = 0.0;
+
+                    for (int k = 0; k < commonSize; k++)
+                    {
+                        c += m1 [i, k] * m2 [k, j];
+                    }
+
+                    result [i, j] = c;
+                }
+            }
+
+            return result;
         }
     }
 }
