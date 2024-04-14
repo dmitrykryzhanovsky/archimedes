4425a33f01c18a1ddd8637273c73770c563c39e6
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-04-14 17:26:12 +0300

    Перенёс сортировку вставкой

---
+++ src/Archimedes.Core.Tests/SortTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class SortTests
+    {
+        [TestMethod ()]
+        public void InsertionSortTest_Array ()
+        {
+            int [] array = new int [] { 21, 41, 59, 26, 41, 58 };
+
+            int [] expectedArray = new int [] { 21, 26, 41, 41, 58, 59 };
+
+            array.InsertionSort ();
+
+            Assert.AreEqual (true, expectedArray.Equals<int> (array));
+        }
+
+        [TestMethod ()]
+        public void InsertionSortTest_Subarray ()
+        {
+            int [] array = new int [] { 21, 41, 59, 26, 41, 58, 73, 42, 69 };
+
+            int [] expectedArray = new int [] { 21, 41, 26, 41, 42, 58, 59, 73, 69 };
+
+            array.InsertionSort (2, 7);
+
+            Assert.AreEqual (true, expectedArray.Equals<int> (array));
+        }
+    }
+}
---
+++ src/Archimedes.Core/Sort.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace Archimedes
+{
+    public static class Sort
+    {
+        /// <summary>
+        /// Сортировка вставкой массива <paramref name="array"/> по возрастанию.
+        /// </summary>
+        public static void InsertionSort<T> (this T [] array) where T : INumber<T>
+        {
+            InsertionSort (array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Сортировка вставкой подмассива <paramref name="array"/> [<paramref name="beginIndex"/> .. <paramref name="endIndex"/>] по 
+        /// возрастанию.
+        /// </summary>
+        public static void InsertionSort<T> (this T [] array, int beginIndex, int endIndex) where T : INumber<T>
+        {
+            for (int i = beginIndex + 1; i <= endIndex; i++)
+            {
+                T   key = array [i];
+                int j   = i - 1;
+                
+                while ((key < array [j]) && (j >= beginIndex))
+                {
+                    array [j + 1] = array [j];
+                    j--;
+                }
+
+                array [j + 1] = key;
+            }
+        }
+    }
+}
