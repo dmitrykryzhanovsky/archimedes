657b6423fd076138c5488def7129c6a0e3e067ef
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-07-13 14:56:31 +0300

    Commit

--- src/Archimedes.Core/Kit.cs
+++ src/Archimedes.Core/Kit.cs
@@ -1,4 +1,7 @@
-﻿namespace Archimedes
+﻿using System;
+using System.Numerics;
+
+namespace Archimedes
 {
     public static class Kit
     {
@@ -15,7 +18,7 @@
         /// </summary>
         public static void Swap<T> (this T [] array, int index1, int index2)
         {
-            Swap (ref array [index1], ref array [index2]);
+            (array [index1], array [index2]) = (array [index2], array [index1]);
         }
 
         /// <summary>
@@ -24,6 +27,11 @@
         public static void Swap<T> (this List<T> list, int index1, int index2)
         {
             (list [index1], list [index2]) = (list [index2], list [index1]);
+        }
+
+        public static void Swap<T> (this T [,] array, int row1, int column1, int row2, int column2)
+        {
+            (array [row1, column1], array [row2, column2]) = (array [row2, column2], array [row1, column1]);
         }
     }
 }
