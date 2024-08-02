bf55eee96c61b51e29e6afdcd3bd25183bf95671
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-08-02 21:45:00 +0300

    Протестировал сравнение 1-мерных массивов

--- src/Archimedes.Kernel/ArrayExtension.cs
+++ src/Archimedes.Kernel/ArrayExtension.cs
@@ -4,6 +4,11 @@ namespace Archimedes
 {
     public static class ArrayExtension
     {
+        /// <summary>
+        /// Возвращает TRUE, если массивы array1 и array2 равны. В противном случае FALSE.
+        /// </summary>
+        /// <remarks>Массивы считаются равными друг другу, если равны их длины, и элементы, содержащиеся на одних и тех же позициях в 
+        /// массивах, имеют равные значения.</remarks>
         public static bool Equals<T> (this T [] array1, T [] array2) where T : INumber<T>
         {
             if (array1.Length == array2.Length)
