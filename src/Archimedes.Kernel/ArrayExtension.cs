ca6847238c9d0b35a350540761fd826bf31cd64f
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-08-02 21:36:52 +0300

    Закодил сравнение 1-мерных массивов, конструкторы и сравнение векторов

---
+++ src/Archimedes.Kernel/ArrayExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Archimedes
+{
+    public static class ArrayExtension
+    {
+        public static bool Equals<T> (this T [] array1, T [] array2) where T : INumber<T>
+        {
+            if (array1.Length == array2.Length)
+            {
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    if (array1 [i] != array2 [i]) return false;
+                }
+
+                return true;
+            }
+
+            else return false;
+        }
+    }
+}
---
+++ src/Archimedes.Kernel/Vector.cs
@@ -0,0 +1,58 @@
+﻿namespace Archimedes
+{
+    public class Vector : ICloneable, IEquatable<Vector>
+    {
+        protected readonly double [] _x;
+
+        public double this [int index]
+        {
+            get => _x [index];
+
+            set => _x [index] = value;
+        }
+
+        public double [] Coordinates
+        {
+            get => _x;
+        }
+
+        public int Dimension
+        {
+            get => _x.Length;
+        }
+
+        public Vector (int dimension)
+        {
+            _x = new double [dimension];
+        }
+
+        public Vector (params double [] x) : this (x.Length)
+        {
+            x.CopyTo (_x, 0);
+        }
+
+        public Vector (Vector other) : this (other._x)
+        {
+        }
+
+        public object Clone ()
+        {
+            return new Vector (this);
+        }
+
+        public bool Equals (Vector? other)
+        {
+            return _x.Equals<double> (other._x);
+        }
+
+        public static bool operator== (Vector v1, Vector v2)
+        {
+            return v1.Equals (v2);
+        }
+
+        public static bool operator != (Vector v1, Vector v2)
+        {
+            return !v1.Equals (v2);
+        }
+    }
+}
