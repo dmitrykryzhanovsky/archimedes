7cdf323d6960243ca97e25819f22f5764fd42fef
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-09-24 20:57:13 +0300

    Добавил операторы для Vector3

--- src/Archimedes.Kernel/Vector3.cs
+++ src/Archimedes.Kernel/Vector3.cs
@@ -73,6 +73,36 @@
 
         #endregion
 
+        public static Vector3 operator + (Vector3 v1, Vector3 v2)
+        {
+            return new Vector3 (v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector3 operator - (Vector3 v1, Vector3 v2)
+        {
+            return new Vector3 (v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public static Vector3 operator - (Vector3 v)
+        {
+            return new Vector3 (-v.X, -v.Y, -v.Z);
+        }
+
+        public static Vector3 operator * (Vector3 v, double coefficient)
+        {
+            return new Vector3 (v.X * coefficient, v.Y * coefficient, v.Z * coefficient);
+        }
+
+        public static Vector3 operator * (double coefficient, Vector3 v)
+        {
+            return v * coefficient;
+        }
+
+        public static Vector3 operator / (Vector3 v, double coefficient)
+        {
+            return new Vector3 (v.X / coefficient, v.Y / coefficient, v.Z / coefficient);
+        }
+
         /// <summary>
         /// Возвращает скалярное произведение векторов.
         /// </summary>
