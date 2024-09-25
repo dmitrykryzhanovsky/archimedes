f6dd78cbdb8f68929f719de047292a886268cea6
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-09-25 21:11:33 +0300

    Добавил операторы для Matrix

--- src/Archimedes.Kernel/Matrix.cs
+++ src/Archimedes.Kernel/Matrix.cs
@@ -72,5 +72,95 @@
         }
 
         #endregion
+
+        public static Matrix operator + (Matrix m1, Matrix m2)
+        {
+            if ((m1.Height == m2.Height) && (m1.Width == m2.Width))
+            {
+                Matrix output = new Matrix (m1.Height, m1.Width);
+
+                for (int i = 0; i < m1.Height; i++)
+                {
+                    for (int j = 0; j < m1.Width; j++)
+                    {
+                        output [i, j] = m1 [i, j] + m2 [i, j];
+                    }
+                }
+
+                return output;
+            }
+
+            else throw new ArithmeticException ();
+        }
+
+        public static Matrix operator - (Matrix m1, Matrix m2)
+        {
+            if ((m1.Height == m2.Height) && (m1.Width == m2.Width))
+            {
+                Matrix output = new Matrix (m1.Height, m1.Width);
+
+                for (int i = 0; i < m1.Height; i++)
+                {
+                    for (int j = 0; j < m1.Width; j++)
+                    {
+                        output [i, j] = m1 [i, j] - m2 [i, j];
+                    }
+                }
+
+                return output;
+            }
+
+            else throw new ArithmeticException ();
+        }
+
+        public static Matrix operator - (Matrix m)
+        {
+            Matrix output = new Matrix (m.Height, m.Width);
+
+            for (int i = 0; i < m.Height; i++)
+            {
+                for (int j = 0; j < m.Width; j++)
+                {
+                    output [i, j] = -m [i, j];
+                }
+            }
+
+            return output;
+        }
+
+        public static Matrix operator * (Matrix m, double coefficient)
+        {
+            Matrix output = new Matrix (m.Height, m.Width);
+
+            for (int i = 0; i < m.Height; i++)
+            {
+                for (int j = 0; j < m.Width; j++)
+                {
+                    output [i, j] = m [i, j] * coefficient;
+                }
+            }
+
+            return output;
+        }
+
+        public static Matrix operator * (double coefficient, Matrix m)
+        {
+            return m * coefficient;
+        }
+
+        public static Matrix operator / (Matrix m, double coefficient)
+        {
+            Matrix output = new Matrix (m.Height, m.Width);
+
+            for (int i = 0; i < m.Height; i++)
+            {
+                for (int j = 0; j < m.Width; j++)
+                {
+                    output [i, j] = m [i, j] / coefficient;
+                }
+            }
+
+            return output;
+        }
     }
 }
