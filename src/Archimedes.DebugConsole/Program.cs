0bcc4ccc97dbd88db3ad98ba073246190407bae9
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-01 21:58:34 +0300

    Обновление кода

--- src/Archimedes.DebugConsole/Program.cs
+++ src/Archimedes.DebugConsole/Program.cs
@@ -1,13 +1,9 @@
-﻿using Archimedes.DebugConsole.Space3;
-
-namespace Archimedes.DebugConsole
+﻿namespace Archimedes.DebugConsole
 {
     internal static class Program
     {
         private static void Main (string [] args)
         {
-            RotationDebug.EulerAnglesVectorPolarUnitPolar ();
-
             Console.ReadKey ();
         }
     }
