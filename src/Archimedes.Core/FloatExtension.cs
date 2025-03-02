ee9c16dba28ae3b814df05757b94c3cc3f74b722
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-02 08:09:40 +0300

    Added fraction part computation

---
+++ src/Archimedes.Core/FloatExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Archimedes
+{
+    public static class FloatExtension
+    {
+        public static T Fraction<T> (this T x) where T : IFloatingPoint<T>
+        {
+            return x - T.Floor (x);
+        }
+    }
+}
