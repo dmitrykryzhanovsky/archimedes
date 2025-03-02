44f8cefce95a313c3c98365a861341c60ea74391
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-02 08:17:09 +0300

    Tested fraction part computation

--- src/Archimedes.Core/FloatExtension.cs
+++ src/Archimedes.Core/FloatExtension.cs
@@ -4,6 +4,9 @@ namespace Archimedes
 {
     public static class FloatExtension
     {
+        /// <summary>
+        /// Возвращает дробную часть вещественного числа.
+        /// </summary>
         public static T Fraction<T> (this T x) where T : IFloatingPoint<T>
         {
             return x - T.Floor (x);
