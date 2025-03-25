8834dd4f45a41a9e40bee1d69c1e08464de48292
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-25 20:27:43 +0300

    Написал конструкторы для Polar2 и Polar3

--- src/Archimedes.Core/Polar2.cs
+++ src/Archimedes.Core/Polar2.cs
@@ -1,25 +1,58 @@
 ﻿namespace Archimedes
 {
-    public class Polar2
+    public class Polar2 : ICloneable
     {
         private double _r;
-
         private double _heading;
 
         public double R
         {
-            get => _r;
+            get
+            {
+                return _r;
+            }
+
+            set
+            {
+                CheckR (value);
+
+                _r = value;
+            }
         }
 
         public double Heading
         {
             get => _heading;
+
+            set => _heading = value;
         }
 
         public Polar2 (double r, double heading)
         {
+            CheckR (r);
+
+            Init (r, heading);
+        }
+
+        public Polar2 (Polar2 other)
+        {
+            Init (other._r, other._heading);
+        }
+
+        public object Clone ()
+        {
+            return new Polar2 (this);
+        }
+
+        private static void CheckR (double r)
+        {
+            if (r < 0.0) throw new ArgumentOutOfRangeException (nameof (r));
+        }
+
+        private void Init (double r, double heading)
+        {
             _r       = r;
             _heading = heading;
-        }
+        }        
     }
 }
---
+++ src/Archimedes.Core/Polar3.cs
@@ -0,0 +1,81 @@
+﻿namespace Archimedes
+{
+    public class Polar3 : ICloneable
+    {
+        private double _r;
+        private double _lat;
+        private double _long;
+
+        public double R
+        {
+            get
+            {
+                return _r;
+            }
+
+            set
+            {
+                CheckR (value);
+
+                _r = value;
+            }
+        }
+
+        public double Lat
+        {
+            get
+            {
+                return _lat;
+            }
+
+            set
+            {
+                CheckLatitude (value);
+
+                _lat = value;
+            }
+        }
+
+        public double Long
+        {
+            get => _long;
+
+            set => _long = value;
+        }
+
+        public Polar3 (double r, double latitude, double longitude)
+        {
+            CheckR (r);
+            CheckLatitude (latitude);
+
+            Init (r, latitude, longitude);
+        }
+
+        public Polar3 (Polar3 other)
+        {
+            Init (other._r, other._lat, other._long);
+        }
+
+        public object Clone ()
+        {
+            return new Polar3 (this);
+        }
+
+        private static void CheckR (double r)
+        {
+            if (r < 0.0) throw new ArgumentOutOfRangeException (nameof (r));
+        }
+
+        private static void CheckLatitude (double latitude)
+        {
+            if ((latitude < -MathConst.Pi_2) || (latitude > MathConst.Pi_2)) throw new ArgumentOutOfRangeException (nameof (latitude));
+        }
+
+        private void Init (double r, double latitude, double longitude)
+        {
+            _r    = r;
+            _lat  = latitude;
+            _long = longitude;
+        }        
+    }
+}
