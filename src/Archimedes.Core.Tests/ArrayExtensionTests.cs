b29357666aa3b1480d8f0031ee1fc39f94856792
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-08-10 21:20:01 +0300

    Написал метод для сравнения 1-мерных массивов. Протестировал его

---
+++ src/Archimedes.Core.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class ArrayExtensionTests
+    {
+        [TestMethod ()]
+        public void EqualsTest_LengthNotEqual ()
+        {
+            int [] array1 = new int [] { 2, 3, 5 };
+            int [] array2 = new int [] { 2, 3, 5, 7 };
+
+            bool expected = false;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void EqualsTest_LengthEqual_ItemsNotEqual ()
+        {
+            int [] array1 = new int [] { 2, 3, 5, 8 };
+            int [] array2 = new int [] { 2, 3, 5, 7 };
+
+            bool expected = false;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void EqualsTest_LengthEqual_ItemsEqual ()
+        {
+            int [] array1 = new int [] { 2, 3, 5, 8 };
+            int [] array2 = new int [] { 2, 3, 5, 8 };
+
+            bool expected = true;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
