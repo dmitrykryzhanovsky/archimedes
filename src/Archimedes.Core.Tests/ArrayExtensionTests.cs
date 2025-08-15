89aa8e46a6009737a1f7c4b8ae7cf5fcafbf0730
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-08-15 18:49:14 +0300

    Написал метод для сравнения 2-мерных массивов. Протестировал его

--- src/Archimedes.Core.Tests/ArrayExtensionTests.cs
+++ src/Archimedes.Core.Tests/ArrayExtensionTests.cs
@@ -6,7 +6,7 @@ namespace Archimedes.Tests
     public class ArrayExtensionTests
     {
         [TestMethod ()]
-        public void EqualsTest_LengthNotEqual ()
+        public void EqualsTest_1D_LengthNotEqual ()
         {
             int [] array1 = new int [] { 2, 3, 5 };
             int [] array2 = new int [] { 2, 3, 5, 7 };
@@ -19,7 +19,7 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void EqualsTest_LengthEqual_ItemsNotEqual ()
+        public void EqualsTest_1D_LengthEqual_ItemsNotEqual ()
         {
             int [] array1 = new int [] { 2, 3, 5, 8 };
             int [] array2 = new int [] { 2, 3, 5, 7 };
@@ -32,10 +32,49 @@ namespace Archimedes.Tests
         }
 
         [TestMethod ()]
-        public void EqualsTest_LengthEqual_ItemsEqual ()
+        public void EqualsTest_1D_LengthEqual_ItemsEqual ()
         {
             int [] array1 = new int [] { 2, 3, 5, 8 };
             int [] array2 = new int [] { 2, 3, 5, 8 };
+
+            bool expected = true;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void EqualsTest_2D_SizeNotEqual ()
+        {
+            int [,] array1 = new int [,] { { 2, 3, 5 }, { 13, 21, 34 } };
+            int [,] array2 = new int [,] { { 2, 3, 5, 7 }, { 11, 13, 17, 19 } };
+
+            bool expected = false;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void EqualsTest_2D_SizeEqual_ItemsNotEqual ()
+        {
+            int [,] array1 = new int [,] { { 2, 3, 5, 8 }, { 13, 21, 34, 55 } };
+            int [,] array2 = new int [,] { { 2, 3, 5, 7 }, { 11, 13, 17, 19 } };
+
+            bool expected = false;
+
+            bool actual = ArrayExtension.Equals (array1, array2);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void EqualsTest_2D_SizeEqual_ItemsEqual ()
+        {
+            int [,] array1 = new int [,] { { 2, 3, 5, 8 }, { 13, 21, 34, 55 } };
+            int [,] array2 = new int [,] { { 2, 3, 5, 8 }, { 13, 21, 34, 55 } };
 
             bool expected = true;
 
--- src/Archimedes.Core/ArrayExtension.cs
+++ src/Archimedes.Core/ArrayExtension.cs
@@ -6,7 +6,7 @@
         /// Поэлементное сравнение одномерных массивов.
         /// </summary>
         /// <returns>Возвращает TRUE, если массивы array1 и array2 имеют одинаковую длину и на одних и тех же позициях содержат 
-        /// одинаковые значения. В противном случае FALSE.</returns>
+        /// одинаковые элементы. В противном случае FALSE.</returns>
         public static bool Equals<T> (this T [] array1, T [] array2)
         {
             if (array1.Length == array2.Length)
@@ -14,6 +14,32 @@
                 for (int i = 0; i < array1.Length; i++)
                 {
                     if (!array1 [i].Equals (array2 [i])) return false;
+                }
+
+                return true;
+            }
+
+            else return false;
+        }
+
+        /// <summary>
+        /// Поэлементное сравнение двумерных массивов.
+        /// </summary>
+        /// <returns>Возвращает TRUE, если массивы array1 и array2 имеют одинаковые длины по обоим измерениям и на одних и тех же 
+        /// позициях содержат одинаковые элементы. В противном случае FALSE.</returns>
+        public static bool Equals<T> (this T [,] array1, T [,] array2)
+        {
+            int height = array1.GetLength (0);
+            int width  = array1.GetLength (1);
+
+            if ((height == array2.GetLength (0)) && (width == array2.GetLength (1)))
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (!array1 [i, j].Equals (array2 [i, j])) return false;
+                    }
                 }
 
                 return true;
