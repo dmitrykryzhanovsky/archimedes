24352d9a98dd054f9b4663fe1ef839d10f5205f2
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-08-15 19:14:27 +0300

    Написал метод для копирования 2-мерных массивов. Пртестировал его

--- src/Archimedes.Core/ArrayExtension.cs
+++ src/Archimedes.Core/ArrayExtension.cs
@@ -13,6 +13,25 @@
         }
 
         /// <summary>
+        /// Копирует двумерный массив source в двумерный массив destination.
+        /// </summary>
+        /// <remarks>Память под массивы source и destination уже должна быть выделена, и длины массива destination по обоим измерениям 
+        /// должны быть не меньше, чем длины массива source по соответствующим измерениям.</remarks>
+        public static void CopyTo<T> (this T [,] source, T [,] destination)
+        {
+            int height = source.GetLength (0);
+            int width  = source.GetLength (1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    destination [i, j] = source [i, j];
+                }
+            }
+        }
+
+        /// <summary>
         /// Поэлементное сравнение одномерных массивов.
         /// </summary>
         /// <returns>Возвращает TRUE, если массивы array1 и array2 имеют одинаковую длину и на одних и тех же позициях содержат 
