b97a5745a39c864fe4cd35462d264ec8832fa7a4
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-10-16 20:50:54 +0300

    Перенёс код и тесты для вычисления полиномов

---
+++ src/Archimedes.Core.Tests/PolynomialTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class PolynomialTests
+    {
+        [TestMethod ()]
+        public void ComputeTest ()
+        {
+            double    x = -2;
+            double [] a = new double [] { 3, -5, 8, -13, 21 };
+
+            double expected = 485;
+
+            double actual = Polynomial.Compute (x, a);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void ComputeSquareTest ()
+        {
+            double    x = -2;
+            double [] a = new double [] { 3, -5, 8 };
+
+            double expected = 45;
+
+            double actual = Polynomial.ComputeSquare (x, a);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void ComputeCubeTest ()
+        {
+            double    x = -2;
+            double [] a = new double [] { 3, -5, 8, -13 };
+
+            double expected = 149;
+
+            double actual = Polynomial.ComputeCube (x, a);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
