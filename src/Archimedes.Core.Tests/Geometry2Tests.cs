87596bcd0a63d44749615d1d11b1c781b6261784
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-11-09 12:14:50 +0300

    Упростил вычисление периметра эллипса (убрал малую полуось b)

--- src/Archimedes.Core.Tests/Geometry2Tests.cs
+++ src/Archimedes.Core.Tests/Geometry2Tests.cs
@@ -9,12 +9,11 @@ namespace Archimedes.Tests
         public void Ellipse_LengthTest ()
         {
             double a        = 10.0;
-            double b        =  8.0;
             double sqrt1me2 =  0.8;
 
             double expected = 56.7233357779486;
 
-            double actual = Geometry2.Ellipse.Length (a, b, sqrt1me2);
+            double actual = Geometry2.Ellipse.Length (a, sqrt1me2);
 
             Assert.AreEqual (expected, actual, 1.0e-14);
         }
--- src/Archimedes.Core/Geometry2.cs
+++ src/Archimedes.Core/Geometry2.cs
@@ -8,16 +8,15 @@
             /// Возвращает периметр эллипса.
             /// </summary>
             /// <param name="a">Большая полуось a.</param>
-            /// <param name="b">Малая полуось b.</param>
             /// <param name="sqrt1me2">Вспомогательная величина sqrt(1 - e^2), где e – эксцентриситет эллипса.</param>
             /// <remarks>Вычисляет приближённое значение периметра Эллипса по второй формуле Рамануджана: 
             /// https://ru.wikipedia.org/wiki/Эллипс#Длина_дуги_эллипса</remarks>
-            public static double Length (double a, double b, double sqrt1me2)
+            public static double Length (double a, double sqrt1me2)
             {
                 double q = (1.0 - sqrt1me2) / (1.0 + sqrt1me2);
                 double s = 3.0 * q * q;
 
-                return double.Pi * (a + b) * (1.0 + s / (10.0 + double.Sqrt (4.0 - s)));
+                return double.Pi * a * (1.0 + sqrt1me2) * (1.0 + s / (10.0 + double.Sqrt (4.0 - s)));
             }
         }
     }
