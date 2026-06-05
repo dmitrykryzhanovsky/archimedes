babf43b615f72b430e54a500fe97a4c2f87cf51e
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2026-06-05 20:55:57 +0300

    Перенос кода

---
+++ src/Archimedes.Core.Tests/SeriesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class SeriesTests
+    {
+        [TestMethod ()]
+        public void SeriesXY02Test ()
+        {
+            double    x =  3;
+            double    y = -5;
+            double [] a =  new double [] { 2, 3, 5, 7, 11, 13 };
+
+            double expected = 181;
+
+            double actual = Series.SeriesXY02 (x, y, a);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void SeriesXY13Test ()
+        {
+            double    x =  3;
+            double    y = -5;
+            double [] a =  new double [] { 2, 3, 5, 7, 11, 13 };
+
+            double expected = -905;
+
+            double actual = Series.SeriesXY13 (x, y, a);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
