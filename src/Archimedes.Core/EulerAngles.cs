fcdf2512dcb947cee1a313ca997d03e8eeb8c08d
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2026-06-05 20:55:57 +0300

    Перенос код

---
+++ src/Archimedes.Core/EulerAngles.cs
@@ -0,0 +1,30 @@
+﻿namespace Archimedes
+{
+    /// <summary>
+    /// Структура для хранения углов Эйлера.
+    /// </summary>
+    public struct EulerAngles
+    {
+        /// <summary>
+        /// Угол прецессии.
+        /// </summary>
+        public double Alpha;
+
+        /// <summary>
+        /// Угол нутации.
+        /// </summary>
+        public double Beta;
+
+        /// <summary>
+        /// Угол собственного вращения.
+        /// </summary>
+        public double Gamma;
+
+        public EulerAngles (double alpha, double beta, double gamma)
+        {
+            Alpha = alpha;
+            Beta  = beta;
+            Gamma = gamma;
+        }
+    }
+}
